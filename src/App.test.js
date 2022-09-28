956b74c311b9486590d07e57f67fbd45a09e8a03
Jake Hong <jake@viralspace.ai>
2022-09-28 15:49:22 -0700

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,7 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// test('test2', () => {
+//   expect(1).toEqual(1)
+// })
